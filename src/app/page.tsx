e0b9157114cb054e0b4df58aa484b8910134f2e9
felipematthew <felipematthewnascimento@gmail.com>
2024-10-12 17:38:29 -0300

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+const page = () => {
+  return (
+    <div className="text-blue-700">page</div>
+  )
+}
+
+export default page 
---
+++ src/app/parent/page.tsx
@@ -0,0 +1,7 @@
+const ParentPage = () => {
+  return (
+    <div className="text-blue-500 font-bold">ParentPage</div>
+  )
+}
+
+export default ParentPage
