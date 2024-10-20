23f5b40d865de8ff802d4cdbc53713fad1d2a7b9
felipematthew <felipematthewnascimento@gmail.com>
2024-10-19 22:33:45 -0300

    feat: table component & all teacher page ✔

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -1,7 +1,90 @@
+import Pagination from "@/src/components/Pagination";
+import Table from "@/src/components/Table";
 import TableSearch from "@/src/components/TableSearch";
+import { role, teachersData } from "@/src/lib/data";
+import { Eye, Trash } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
+
+type TeacherType = {
+  id: number;
+  teacherId: string;
+  name: string;
+  email?: string;
+  photo: string;
+  phone: string;
+  subjects: string[];
+  classes: string[];
+  address: string;
+};
+
+// Responsividade não vai exibir algumas colunas
+const columns = [
+  {
+    header: "Info",
+    accessor: "info",
+  },
+  {
+    header: "Teacher ID",
+    accessor: "teacherId",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Subjects",
+    accessor: "subjects",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Classes",
+    accessor: "classes",
+    className: "hidden lg:table-cell",
+  },
+  {
+    header: "Phone",
+    accessor: "phone",
+    className: "hidden lg:table-cell",
+  },
+  {
+    header: "Address",
+    accessor: "address",
+    className: "hidden xl:table-cell",
+  },
+  {
+    header: "Actions",
+    accessor: "actions",
+  },
+];
 
 const TeacherListPage = () => {
+  const renderRow = (item : TeacherType) => (
+    <tr key={item.id} className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-purple-50 duration-300 cursor-pointer">
+      <td className="flex items-center gap-4 p-4">
+        <Image src={item.photo} alt={item.name} width={40} height={40} className="md:hidden xl:block w-10 h-10 rounded-full object-cover"/>
+        <div className="flex flex-col">
+          <h3 className="font-semibold">{item.name}</h3>
+          <p className="text-xs text-gray-500">{item?.email}</p>
+        </div>
+      </td>
+      <td className="hidden md:table-cell">{item.teacherId}</td>
+      <td className="hidden md:table-cell">{item.subjects.join(", ")}</td>
+      <td className="hidden lg:table-cell">{item.classes.join(", ")}</td>
+      <td className="hidden lg:table-cell">{item.phone}</td>
+      <td className="hidden xl:table-cell">{item.address}</td>
+      <td>
+        <div className="flex items-center gap-2">
+          <Link href={`/list/teachers/${item.id}`}>
+          <button type="button" className="w-7 h-7 flex items-center justify-center rounded-full bg-blue-300">
+            <Eye width={16} height={16} className="text-white"/>
+          </button>
+          </Link>
+          {role === "admin" && <button  type="button" className="w-7 h-7 flex items-center justify-center rounded-full bg-purple-300">
+            <Trash width={16} height={16} className="text-white"/>
+          </button>}
+        </div>
+      </td>
+    </tr>
+  );
+
   return (
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
       {/* TOP */}
@@ -10,22 +93,22 @@ const TeacherListPage = () => {
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch />
           <div className="flex items-center gap-4 self-end">
-            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-blue-100">
+            <button type="button" className="w-8 h-8 flex items-center justify-center rounded-full bg-blue-100">
               <Image src="/filter.png" width={14} height={14} alt="" />
             </button>
-            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-blue-100">
+            <button type="button" className="w-8 h-8 flex items-center justify-center rounded-full bg-blue-100">
               <Image src="/sort.png" width={14} height={14} alt="" />
             </button>
-            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-blue-100">
+            <button type="button" className="w-8 h-8 flex items-center justify-center rounded-full bg-blue-100">
               <Image src="/plus.png" width={14} height={14} alt="" />
             </button>
           </div>
         </div>
       </div>
       {/* LIST */}
-      <div></div>
+      <Table columns={columns} renderRow={renderRow} data={teachersData}/>
       {/* PAGINATION */}
-      <div></div>
+      <Pagination />
     </div>
   );
 };
--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,8 +1,24 @@
-
 const Pagination = () => {
   return (
-    <div>Pagination</div>
-  )
-}
+    <div className="p-4 flex items-center justify-between text-gray-500">
+      <button disabled type="button" className="py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed">
+        Prev
+      </button>
 
-export default Pagination
+      <div className="flex items-center gap-2 text-sm">
+        <button type="button" className="px-2 rounded bg-blue-200">1</button>
+        <button type="button" className="px-2 rounded">2</button>
+        <button type="button" className="px-2 rounded">3</button>
+        ...
+        <button type="button" className="px-2 rounded">10</button>
+
+      </div>
+
+      <button type="button" className="py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed">
+        Next
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
