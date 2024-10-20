39ad387f8569737c6080e3737b85257d257d8f68
felipematthew <felipematthewnascimento@gmail.com>
2024-10-19 23:45:25 -0300

    feat: list page teachers ✔

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -56,10 +56,19 @@ const columns = [
 ];
 
 const TeacherListPage = () => {
-  const renderRow = (item : TeacherType) => (
-    <tr key={item.id} className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-purple-50 duration-300 cursor-pointer">
+  const renderRow = (item: TeacherType) => (
+    <tr
+      key={item.id}
+      className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-purple-50 duration-300 cursor-pointer"
+    >
       <td className="flex items-center gap-4 p-4">
-        <Image src={item.photo} alt={item.name} width={40} height={40} className="md:hidden xl:block w-10 h-10 rounded-full object-cover"/>
+        <Image
+          src={item.photo}
+          alt={item.name}
+          width={40}
+          height={40}
+          className="md:hidden xl:block w-10 h-10 rounded-full object-cover"
+        />
         <div className="flex flex-col">
           <h3 className="font-semibold">{item.name}</h3>
           <p className="text-xs text-gray-500">{item?.email}</p>
@@ -73,13 +82,21 @@ const TeacherListPage = () => {
       <td>
         <div className="flex items-center gap-2">
           <Link href={`/list/teachers/${item.id}`}>
-          <button type="button" className="w-7 h-7 flex items-center justify-center rounded-full bg-blue-300">
-            <Eye width={16} height={16} className="text-white"/>
-          </button>
+            <button
+              type="button"
+              className="w-7 h-7 flex items-center justify-center rounded-full bg-blue-300"
+            >
+              <Eye width={16} height={16} className="text-white" />
+            </button>
           </Link>
-          {role === "admin" && <button  type="button" className="w-7 h-7 flex items-center justify-center rounded-full bg-purple-300">
-            <Trash width={16} height={16} className="text-white"/>
-          </button>}
+          {role === "admin" && (
+            <button
+              type="button"
+              className="w-7 h-7 flex items-center justify-center rounded-full bg-purple-300"
+            >
+              <Trash width={16} height={16} className="text-white" />
+            </button>
+          )}
         </div>
       </td>
     </tr>
@@ -93,20 +110,31 @@ const TeacherListPage = () => {
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch />
           <div className="flex items-center gap-4 self-end">
-            <button type="button" className="w-8 h-8 flex items-center justify-center rounded-full bg-blue-100">
+            <button
+              type="button"
+              className="w-8 h-8 flex items-center justify-center rounded-full bg-blue-100"
+            >
               <Image src="/filter.png" width={14} height={14} alt="" />
             </button>
-            <button type="button" className="w-8 h-8 flex items-center justify-center rounded-full bg-blue-100">
+            <button
+              type="button"
+              className="w-8 h-8 flex items-center justify-center rounded-full bg-blue-100"
+            >
               <Image src="/sort.png" width={14} height={14} alt="" />
             </button>
-            <button type="button" className="w-8 h-8 flex items-center justify-center rounded-full bg-blue-100">
-              <Image src="/plus.png" width={14} height={14} alt="" />
-            </button>
+            {role === "admin" && (
+              <button
+                type="button"
+                className="w-8 h-8 flex items-center justify-center rounded-full bg-blue-100"
+              >
+                <Image src="/plus.png" width={14} height={14} alt="" />
+              </button>
+            )}
           </div>
         </div>
       </div>
       {/* LIST */}
-      <Table columns={columns} renderRow={renderRow} data={teachersData}/>
+      <Table columns={columns} renderRow={renderRow} data={teachersData} />
       {/* PAGINATION */}
       <Pagination />
     </div>
