54440a06f2262bea795ee0a4141c1870cf66b1fb
felipematthew <felipematthewnascimento@gmail.com>
2024-10-21 22:17:08 -0300

    wip: form modal

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { Pencil, Plus, Trash } from "lucide-react";
+import { Pencil, Plus, Trash, X } from "lucide-react";
+import { useState } from "react";
 
 // TODO: PROPS
 const FormModal = ({
@@ -27,28 +28,57 @@ const FormModal = ({
   id?: number;
 }) => {
   const size = type === "create" ? "w-8 h-8" : "w-7 h-7";
+
   const bgColor =
     type === "create"
       ? "bg-blue-100"
       : type === "update"
       ? "bg-blue-400"
       : "bg-purple-300";
+
   const icon =
     type === "create" ? (
-      <Plus width={16} height={16}/>
+      <Plus width={16} height={16} />
     ) : type === "update" ? (
-      <Pencil width={16} height={16} className="text-white"/>
+      <Pencil width={16} height={16} className="text-white" />
     ) : (
-      <Trash width={16} height={16} className="text-white"/>
+      <Trash width={16} height={16} className="text-white" />
     );
+
+  const [open, setOpen] = useState<boolean>(false);
+
+  const Form = () => {
+    return type === "delete" && id ? <form action="" className="p-4 flex flex-col gap-4">
+      <span className="text-center font-medium">All data will be lost. Are you sure want to delete this {table}?</span>
+      <button type="button" className="bg-red-700 text-white py-2 px-4 rounded-md border-none w-max self-center">Delete</button>
+    </form> : ""
+  }
 
   return (
     <>
       <button
         type="button"
         className={`${size} flex items-center justify-center rounded-full ${bgColor}`}
+        onClick={() => setOpen(true)}
       >
         {icon}
+        {open && (
+          <div className="w-screen h-screen absolute left-0 top-0 bg-black bg-opacity-60 z-50 flex items-center justify-center">
+            <div
+              className="bg-white p-4 shadow relative w-[90%] md:w-[70%] lg:w-[60%]
+            xl:w-[50%]  2xl:w-[40%] rounded-md"
+            >
+              <Form/>
+              <div
+                className="absolute top-4 right-4 cursor-pointer"
+                onClick={() => setOpen(false)}
+                onKeyDown={() => setOpen(false)}
+              >
+                <X className="text-black" width={18} height={18} />
+              </div>
+            </div>
+          </div>
+        )}
       </button>
     </>
   );
